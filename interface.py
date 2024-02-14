ddf5fcad97b0d81eceede6012603066636e610a4
ORKHAN <orkhan.ismayilov05@gmail.com>
2024-02-14 13:44:59 +0400

    Initial commit

--- interface.py
+++ interface.py
@@ -21,24 +21,6 @@ class Interface:
             print("User already exists.")
 
 
-#Создаем экземпляр интерфейса
-interface = Interface()
-
-# Регистрируем нового пользователя
-interface.register_user("Orkhan", "Ismayilov", "12345")
-
-# Входим в систему под созданным пользователем
-user = interface.login("Orkhan", "12345")
-
-# Если вход успешен, добавляем событие в календарь пользователя
-if user:
-    user.create_calendar()
-    user.create_event("event1", "Встреча", "Обсудить проект", ["Orkhan"])
-
-user.create_event("event1", "Встреча", "Обсудить проект", ["Orkhan"])
-events = user.get_calendar().get_events()
-for event_id, event_data in events.items():
-    print(f"Event ID: {event_id}, Title: {event_data['title']}, Description: {event_data['description']}, Participants: {', '.join(event_data['participants'])}")
 
 
 
